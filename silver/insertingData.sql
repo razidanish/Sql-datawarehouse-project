e93dd67739f9b7ebe4eff23d3b11ba1496a2d795
razidanish <144589297+razidanish@users.noreply.github.com>
2025-12-01 13:00:22 +0530

    Update insertingData.sql

--- silver/insertingData.sql
+++ silver/insertingData.sql
@@ -96,47 +96,43 @@ CREATE TABLE silver.crm_sales_details (
 );
 
 INSERT INTO silver.crm_sales_details (
-   sls_ord_num,
-   sls_prd_key,
-   sls_cust_id,
-   sls_order_dt,
-   sls_ship_dt,
-   sls_due_dt,
-   sls_sales,
-   sls_quantity,
-   sls_price
-   )
+			sls_ord_num,
+			sls_prd_key,
+			sls_cust_id,
+			sls_order_dt,
+			sls_ship_dt,
+			sls_due_dt,
+			sls_sales,
+			sls_quantity,
+			sls_price
+		)
 SELECT 
 sls_ord_num,
 sls_prd_key,
-sls_quantity,
+sls_cust_id,
 CASE 
- WHEN sls_order_dt <=0 OR LEN(sls_order_dt) !=8 THEN NULL
- ELSE CAST(CAST(sls_order_dt as VARCHAR) AS DATE)
- END as sls_order_dt,
+ WHEN sls_order_dt = 0 or LEN(sls_order_dt) != 8 THEN NULL
+ ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE) 
+ END AS sls_order_dt,
 CASE 
- WHEN sls_ship_dt <=0 OR LEN(sls_ship_dt) !=8 THEN NULL
- ELSE CAST(CAST(sls_ship_dt as VARCHAR) AS DATE)
- END as sls_ship_dt,
-CASE 
- WHEN sls_due_dt <=0 OR LEN(sls_due_dt) !=8 THEN NULL
- ELSE CAST(CAST(sls_due_dt as VARCHAR) AS DATE)
- END as sls_due_dt,
+ WHEN sls_ship_dt = 0 OR LEN(sls_ship_dt) != 8 THEN NULL
+ ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE) 
+ END AS sls_ship_dt,
 CASE
- WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price)
- THEN sls_quantity * ABS(sls_price)
- ELSE sls_sales
- END AS sls_sales,
-CASE
- WHEN sls_quantity IS NULL OR sls_quantity <= 0 OR sls_quantity != sls_sales / ABS(sls_price)
- THEN sls_sales / ABS(sls_price)
- ELSE sls_quantity
- END AS sls_quantity,
-CASE
- WHEN sls_price IS NULL OR sls_price <= 0 OR sls_price != sls_sales / sls_quantity
- THEN sls_sales / NULLIF(sls_quantity,0)
- ELSE sls_price
- END AS sls_price
+ WHEN sls_due_dt = 0 OR LEN(sls_due_dt) != 8 THEN NULL
+ ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE) 
+ END AS sls_due_dt,
+ CASE
+  WHEN sls_sales IS NULL OR sls_sales <= 0 or sls_sales != sls_quantity * ABS(sls_price)
+  THEN sls_quantity * ABS(sls_price)
+  ELSE sls_sales
+  END AS sls_sales,
+  sls_quantity,
+ CASE
+  WHEN sls_price IS NULL OR sls_price <= 0 
+  THEN sls_sales / NULLIF(sls_quantity,0)
+  ELSE sls_price
+  END AS sls_price
 FROM bronze.crm_sales_details;
 
 
